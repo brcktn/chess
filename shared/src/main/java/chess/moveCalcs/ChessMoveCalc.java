4830c5ef0943eb9ab22959594e3ce0871dd5d7e8
brcktn <brcktn@Brocktons-Air-2.app.byu.edu>
2024-06-26 19:03:05 -0600

    Implemented knightMoves()

--- shared/src/main/java/chess/moveCalcs/ChessMoveCalc.java
+++ shared/src/main/java/chess/moveCalcs/ChessMoveCalc.java
@@ -14,7 +14,7 @@ public class ChessMoveCalc {
             case KING -> KingMovesCalc.kingMoves(board, myPosition);
             case QUEEN -> queenMoves(board, myPosition);
             case BISHOP -> BishopMoveCalc.bishopMoves(board, myPosition);
-            case KNIGHT -> knightMoves(board, myPosition);
+            case KNIGHT -> KnightMovesCalc.knightMoves(board, myPosition);
             case ROOK -> rookMoves(board, myPosition);
             case PAWN -> pawnMoves(board, myPosition);
         };
@@ -24,16 +24,23 @@ public class ChessMoveCalc {
         throw new RuntimeException("Not implemented");
     }
 
-    private static Collection<ChessMove> knightMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
-    }
-
     private static Collection<ChessMove> rookMoves(ChessBoard board, ChessPosition myPosition) {
         throw new RuntimeException("Not implemented");
     }
 
     private static Collection<ChessMove> pawnMoves(ChessBoard board, ChessPosition myPosition) {
         throw new RuntimeException("Not implemented");
+    }
+
+    public static void addIfAllowed(Collection<ChessMove> moves, ChessBoard board, ChessPosition currPosition, int i, int j) {
+        if (i >= 1 && i <= 8 && j >= 1 && j <= 8) {
+            ChessPosition newPosition = new ChessPosition(i, j);
+            ChessPiece blockingPiece = board.getPiece(newPosition);
+            ChessPiece currPiece = board.getPiece(currPosition);
+            if (blockingPiece == null || currPiece.getTeamColor() != blockingPiece.getTeamColor()) {
+                moves.add(new ChessMove(currPosition, newPosition, null));
+            }
+        }
     }
 }
 
--- shared/src/main/java/chess/moveCalcs/KingMovesCalc.java
+++ shared/src/main/java/chess/moveCalcs/KingMovesCalc.java
@@ -14,26 +14,15 @@ public class KingMovesCalc {
         int currRow = currPosition.getRow();
         int currCol = currPosition.getColumn();
 
-        addIfAllowed(moves, board, currPosition, currRow+1, currCol);
-        addIfAllowed(moves, board, currPosition, currRow-1, currCol);
-        addIfAllowed(moves, board, currPosition, currRow, currCol+1);
-        addIfAllowed(moves, board, currPosition, currRow, currCol-1);
-        addIfAllowed(moves, board, currPosition, currRow+1, currCol+1);
-        addIfAllowed(moves, board, currPosition, currRow-1, currCol+1);
-        addIfAllowed(moves, board, currPosition, currRow+1, currCol-1);
-        addIfAllowed(moves, board, currPosition, currRow-1, currCol-1);
+        ChessMoveCalc.addIfAllowed(moves, board, currPosition, currRow+1, currCol);
+        ChessMoveCalc.addIfAllowed(moves, board, currPosition, currRow-1, currCol);
+        ChessMoveCalc.addIfAllowed(moves, board, currPosition, currRow, currCol+1);
+        ChessMoveCalc.addIfAllowed(moves, board, currPosition, currRow, currCol-1);
+        ChessMoveCalc.addIfAllowed(moves, board, currPosition, currRow+1, currCol+1);
+        ChessMoveCalc.addIfAllowed(moves, board, currPosition, currRow-1, currCol+1);
+        ChessMoveCalc.addIfAllowed(moves, board, currPosition, currRow+1, currCol-1);
+        ChessMoveCalc.addIfAllowed(moves, board, currPosition, currRow-1, currCol-1);
 
         return moves;
-    }
-
-    private static void addIfAllowed(Collection<ChessMove> moves, ChessBoard board, ChessPosition currPosition, int i, int j) {
-        if (i >= 1 && i <= 8 && j >= 1 && j <= 8) {
-            ChessPosition newPosition = new ChessPosition(i, j);
-            ChessPiece blockingPiece = board.getPiece(newPosition);
-            ChessPiece currPiece = board.getPiece(currPosition);
-            if (blockingPiece == null || currPiece.getTeamColor() != blockingPiece.getTeamColor()) {
-                moves.add(new ChessMove(currPosition, newPosition, null));
-            }
-        }
     }
 }
