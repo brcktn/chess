6d40aee0d532e2a1d57f5409afbcdea538ec560a
brcktn <29804204+brcktn@users.noreply.github.com>
2024-07-18 19:01:40 -0600

    implemented endpoints

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,4 +1,5 @@
 import chess.*;
+import spark.Spark;
 
 public class Main {
     public static void main(String[] args) {
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,10 @@
 package server;
 
+import com.google.gson.Gson;
 import spark.*;
+import java.util.*;
+
+import java.util.HashMap;
 
 public class Server {
 
@@ -9,10 +13,17 @@ public class Server {
 
         Spark.staticFiles.location("web");
 
-        // Register your endpoints and handle exceptions here.
+        // Register your server.endpoints and handle exceptions here.
 
-        //This line initializes the server and can be removed once you have a functioning endpoint 
-        Spark.init();
+        //This line initializes the server and can be removed once you have a functioning endpoint
+
+        Spark.delete("/db", this::clearDatabase);
+        Spark.post("/user", this::registerUser);
+        Spark.post("/session", this::login);
+        Spark.delete("/session", this::logout);
+        Spark.get("/game", this::listGames);
+        Spark.post("/game", this::createGame);
+        Spark.put("/game", this::joinGame);
 
         Spark.awaitInitialization();
         return Spark.port();
@@ -22,4 +33,33 @@ public class Server {
         Spark.stop();
         Spark.awaitStop();
     }
+
+    private Object clearDatabase(Request request, Response response) {
+        return null;
+    }
+
+    private Object registerUser(Request request, Response response) {
+        return null;
+    }
+
+    private Object login(Request request, Response response) {
+        return null;
+    }
+
+    private Object logout(Request request, Response response) {
+        return null;
+    }
+
+    private Object listGames(Request request, Response response) {
+        return null;
+    }
+
+    private Object createGame(Request request, Response response) {
+        return null;
+    }
+
+    private Object joinGame(Request request, Response response) {
+        return null;
+    }
+
 }
